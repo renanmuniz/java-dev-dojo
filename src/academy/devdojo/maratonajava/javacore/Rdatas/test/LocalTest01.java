0b6ed6290886a0e1c027f298a2a52a28af87abe6
renanmuniz <renan.mmerlo@gmail.com>
2024-10-18 23:18:11 -0300

    Ate aula 115

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocalTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it","IT");
+        Locale localeCH = new Locale("it","CH");
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeCH);
+        System.out.println(df1.format(calendar.getTime()));
+        System.out.println(df2.format(calendar.getTime()));
+    }
+}
